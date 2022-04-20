136a2bf9f1e80862535b5e41ffe8240827e0ef9e
asxzq <asxzq000617@163.com>
2022-04-20 16:56:13 +0800

    一点改动

--- main.cpp
+++ main.cpp
@@ -8,23 +8,24 @@ int main() {
 
 	cout << "hello world" << endl;
 
-	string pathNameOBJ1 = "F:\\code\\DS\\MeshSimplification\\data\\data_basic2\\serapis_88040.obj";
+	string pathNameOBJ1 = "E:\Code\C++_VS\MashSimplification\MashSimplification";
 	string pathNameOBJ2 = "F:\\code\\DS\\MeshSimplification\\result\\data_basic2\\serapis_88040.obj";
 	string pathNameOBJ3 = "F:\\code\\DS\\MeshSimplification\\result\\data_basic2\\serapis_88040.obj_true.obj";
 
 	//string inFold = "F:\\code\\DS\\MeshSimplification\\data\\data_advanced1\\tortuga_152728.obj";
-	string inFold = "F:\\code\\DS\\MeshSimplification\\data\\data_basic1\\Utah_teapot_9438.obj";
+	string inFold = "E:\\Code\\C++_VS\\MashSimplification\\MashSimplification\\data\\angel.obj";
 	//string outFold = "F:\\code\\DS\\MeshSimplification\\result\\data_advanced1\\tortuga_152728.obj";
-	string outFold = "F:\\code\\DS\\MeshSimplification\\result\\data_basic1\\Utah_teapot_9438.obj";
+	string outFold = "E:\\Code\\C++_VS\\MashSimplification\\MashSimplification\\result\\angel.obj";
 
 
 
 	MeshSimplification *solution = new MeshSimplification();
-	solution->fileRead(inFold);
 	clock_t start = clock();
+	solution->fileRead(inFold);
+	
 	
 	//basic2 & advanved1
-	//solution->edgeCollapse(300);
+	solution->edgeCollapse(1000);
 	//basic1
 	//solution->edgeCollapseSingle(1923, 3641);
 	
